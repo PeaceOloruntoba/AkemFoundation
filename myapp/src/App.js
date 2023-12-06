6e9d350d9683c404fd60fd0852aa0c3d5b390e71
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-12-06 18:20:12 +0100

    Fixed a little bit of routing

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,6 +4,10 @@ import { Route, Routes } from "react-router-dom";
 import About from './pages/About';
 import Home from './pages/Home';
 import Notfound from './pages/Notfound';
+import Contact from './pages/Contact';
+import Support from './pages/Support';
+import Events from './pages/Events';
+import News from './pages/News';
 
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path='/*' element={<Notfound />}/>
       </Routes>
     </div>
--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -8,11 +8,12 @@ export default function Navbar() {
         <p>Akem</p>
         </div>
         <ul className="flex justify-between gap-12">
-            <li><a href="">Home</a></li>
-            <li><a href="">Home</a></li>
-            <li><a href="">Home</a></li>
-            <li><a href="">Home</a></li>
-            <li><a href="">Home</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">Who We Are</a></li>
+            <li><a href="/support">Support Us</a></li>
+            <li><a href="/events">Events</a></li>
+            <li><a href="/news">News</a></li>
+            <li><a href="/contact">Contact</a></li>
         </ul>
         <a href=""><button className='bg-[#F4CE50] p-8 px-16'>Donate</button></a>
     </div>
