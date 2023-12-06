3f9646e8c59e6f46447ac3af2f95dff45a38d896
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-12-06 18:13:38 +0100

    nghfc

--- myapp/src/components/Navbar.jsx
+++ myapp/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div className='flex text-xl items-center justify-between font-semibold'>
+    <div className='flex text-xl items-center justify-between font-semibold bg-[rgb(37,65,81)] text-white'>
         <div className="flex p-4">
         <img src="" alt="" />
         <p>Akem</p>
@@ -14,7 +14,7 @@ export default function Navbar() {
             <li><a href="">Home</a></li>
             <li><a href="">Home</a></li>
         </ul>
-        <a href=""><button className='bg-green-600 p-8 px-16'>Donate</button></a>
+        <a href=""><button className='bg-[#F4CE50] p-8 px-16'>Donate</button></a>
     </div>
   )
 }
