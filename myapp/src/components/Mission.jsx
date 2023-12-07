e9b965a716b3a61c1722908f86cd366845c2b2d7
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-12-07 07:07:00 +0100

    vb,

--- myapp/src/components/Mission.jsx
+++ myapp/src/components/Mission.jsx
@@ -7,14 +7,16 @@ export default function Mission() {
             <div className="grid bg-[#eaeae8] px-32 py-16 items-center justify-center text-center">
                 <p className='uppercase font-semibold text-5xl py-4 px-8 '>our mission</p>
                 <p className='uppercase font-semibold text-2xl py-4 px-8'>To empower individuals and communities through access to quality education and the development of entrepreneurial skills, fostering brighter futures and self-sustainability in underprivileged communities"</p>
-                <a href="/" className='text-xl text-purple-800 underline'>Read More</a>
+                <a href="/" className='text-xl text-{#45b29d} underline'>Read More</a>
             </div>
-            <div className="grid bg-[#]">
+            <div className="grid bg-[#8e372c]">
                 <p>Take a pledge</p>
                 <a href="/"><button className='bg-white text-transparent'>Support Us</button></a>
             </div>
         </div>
+        45b29d
         <div className="flex"></div>
+        254151
     </div>
   )
 }
